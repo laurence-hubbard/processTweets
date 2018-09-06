479766df04bb9a8b969e49e4b81556edca16159e
laurence-hubbard <laurence.hubbard@comparethemarket.com>
2018-09-06 15:27:52 +0100

    Adding batch power!

---
+++ batch-tweets.py
@@ -0,0 +1,41 @@
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+import tweepy
+import csv
+
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+from twits.es import ESClient
+from twits.modeller import Modeller
+
+import json
+
+falcon = Modeller()
+dobbins = ESClient()
+
+consumer_key = config['twits']['consumer_key']
+consumer_secret = config['twits']['consumer_secret']
+access_token = config['twits']['access_token']
+access_secret = config['twits']['access_secret']
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth,wait_on_rate_limit=True)
+
+csvFile = open('ua.csv', 'a')
+csvWriter = csv.writer(csvFile)
+
+search = config['twits']['search'].split(',')
+
+for item in search:
+  for tweet in tweepy.Cursor(api.search,q=search,count=100,
+                           lang="en",
+                           since="2017-04-03").items():
+    model = falcon.model_tweet(json.loads(tweet._json), [search])
+    if model is not None:
+      dobbins.insert_entry(model)
