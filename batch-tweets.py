0bebe3f62c2937473e775c15b98fcb5ad79713db
laurence-hubbard <laurence.hubbard@comparethemarket.com>
2018-09-06 16:02:12 +0100

    removing unused code

--- batch-tweets.py
+++ batch-tweets.py
@@ -38,12 +38,6 @@ search = ['hello']
 
 from datetime import datetime
 
-dt_obj = datetime.strptime('20.12.2016 09:38:42,76',
-                           '%d.%m.%Y %H:%M:%S,%f')
-millisec = dt_obj.timestamp() * 1000
-
-print(millisec)
-
 def timestamp(self):
         "Return POSIX timestamp as float"
         if self._tzinfo is None:
