4674e3872b1a3ca90bc12ae9092d22ded932b5ef
laurence-hubbard <laurence.hubbard@comparethemarket.com>
2018-09-06 16:58:10 +0100

    improving secondswq

--- batch-tweets.py
+++ batch-tweets.py
@@ -53,5 +53,4 @@ for item in search:
     tweet_info['timestamp_ms'] = (datetime.strptime(tweet_info['created_at'].replace('+0000 ',''),'%a %b %d %X %Y')-datetime(1970,1,1)).total_seconds() * 1000
     model = falcon.model_tweet(tweet_info, [item])
     if model is not None:
-      print(model)
       dobbins.insert_entry(model)
