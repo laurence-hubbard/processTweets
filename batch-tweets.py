eca8d59538bfacb847bef0be155ef07fe3b4d802
laurence-hubbard <laurence.hubbard@comparethemarket.com>
2018-09-06 16:07:57 +0100

    updating timestamp format

--- batch-tweets.py
+++ batch-tweets.py
@@ -52,7 +52,7 @@ for item in search:
                            since="2018-08-27").items():
     tweet_info = tweet._json.copy()
 #    tweet_info['timestamp_ms'] = timestamp(datetime.strptime(tweet_info['created_at'],'%a %b %m %X +0000 %Y')) * 1000
-    tweet_info['timestamp_ms'] = (datetime.strptime(tweet_info['created_at'],'%a %b %m %X +0000 %Y')-datetime.datetime(1970,1,1)).total_seconds()
+    tweet_info['timestamp_ms'] = (datetime.strptime(tweet_info['created_at'],'%a %b %m %X +0000 %Y')-datetime(1970,1,1)).total_seconds()
     model = falcon.model_tweet(json.loads(tweet._json), [search])
     if model is not None:
       dobbins.insert_entry(model)
